906a0de1149ebb05f355ffee34d73c32f3b62f0e
Petr Gregor <petr.gregor@email.cz>
2024-10-01 18:30:22 +0200

    MySQL - task 4 (get_instruments_count).

--- p01_mysql/p09_task_4.py
+++ p01_mysql/p09_task_4.py
@@ -5,7 +5,26 @@ Funkce by měla jako argument přijmout spojení.
 Vrátí záznamy tvořené slovníky se dvěma klíči - 'family' a 'count'.
 """
 
+from mysql.connector import connect, Error
+from connection_details import *
+
 
 def get_instruments_count(connection):
-    # TODO: get_instruments_count
-    pass
+    with connection.cursor(dictionary=True) as cursor:
+        sql_statement = """
+            SELECT family, count(*) as count
+            FROM instruments
+            GROUP BY family;
+        """
+        cursor.execute(sql_statement)
+        return cursor.fetchall()
+
+
+try:
+    with connect(host=host, user=user, password=password, database='music') as conn:
+        result = get_instruments_count(conn)
+        for instrument in result:
+            print(instrument)
+
+except Error as e:
+    print(e)
